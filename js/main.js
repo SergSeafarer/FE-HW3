53ae4dec0e5c6ec56b7eed4e7aeace2ba8d7a143
Sergii <seregaonma@gmail.com>
2023-09-26 13:15:59 +0300

    correct js code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const choice = prompt('What would you like to do: add? sub? mult? div?');
-const firsNumber = prompt('Enter first number');
-const secondNumber = prompt('Enter second nummber');
 if(choice === 'add') {
+  const firsNumber = prompt('Enter first number');
+  const secondNumber = prompt('Enter second nummber');
   const sum = Number(firsNumber) + Number(secondNumber);
   alert(firsNumber + '+' + secondNumber + '=' + sum);
 } else if(choice === 'sub') {
+  const firsNumber = prompt('Enter first number');
+  const secondNumber = prompt('Enter second nummber');
   const sub = firsNumber - secondNumber;
   alert(firsNumber + '-' + secondNumber + '=' + sub);
 } else if(choice === 'mult') {
+  const firsNumber = prompt('Enter first number');
+  const secondNumber = prompt('Enter second nummber');
   const mult = firsNumber * secondNumber;
   alert(firsNumber + '*' + secondNumber + '=' + mult);
 } else if(choice === 'div') {
+  const firsNumber = prompt('Enter first number');
+  const secondNumber = prompt('Enter second nummber');
   const div = firsNumber / secondNumber;
   alert(firsNumber + '/' + secondNumber + '=' + div);
 } else {
