2b774093c627cfa17c1fa4e2281eeff792eef077
Sergii <seregaonma@gmail.com>
2023-09-26 19:21:44 +0300

    add aditional checks for entered numbers

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
+const firstNumber = +prompt('Enter first number');
+const secondNumber = +prompt('Enter second number');
 const choice = prompt('What would you like to do: add? sub? mult? div?');
-if(choice === 'add') {
-  const firsNumber = prompt('Enter first number');
-  const secondNumber = prompt('Enter second nummber');
-  const sum = Number(firsNumber) + Number(secondNumber);
-  alert(firsNumber + '+' + secondNumber + '=' + sum);
-} else if(choice === 'sub') {
-  const firsNumber = prompt('Enter first number');
-  const secondNumber = prompt('Enter second nummber');
-  const sub = firsNumber - secondNumber;
-  alert(firsNumber + '-' + secondNumber + '=' + sub);
-} else if(choice === 'mult') {
-  const firsNumber = prompt('Enter first number');
-  const secondNumber = prompt('Enter second nummber');
-  const mult = firsNumber * secondNumber;
-  alert(firsNumber + '*' + secondNumber + '=' + mult);
-} else if(choice === 'div') {
-  const firsNumber = prompt('Enter first number');
-  const secondNumber = prompt('Enter second nummber');
-  const div = firsNumber / secondNumber;
-  alert(firsNumber + '/' + secondNumber + '=' + div);
+if(choice === 'add' && isNaN(firstNumber) === false && isNaN(secondNumber) === false) {
+  const sum = firstNumber + secondNumber;
+  alert(firstNumber + '+' + secondNumber + '=' + sum);
+} else if(choice === 'sub' && isNaN(firstNumber) === false && isNaN(secondNumber) === false) {
+  const sub = firstNumber - secondNumber;
+  alert(firstNumber + '-' + secondNumber + '=' + sub);
+} else if(choice === 'mult' && isNaN(firstNumber) === false && isNaN(secondNumber) === false) {
+  const mult = firstNumber * secondNumber;
+  alert(firstNumber + '*' + secondNumber + '=' + mult);
+} else if(choice === 'div' && isNaN(firstNumber) === false && isNaN(secondNumber) === false) {
+  const div = firstNumber / secondNumber;
+  alert(firstNumber + '/' + secondNumber + '=' + div);
 } else {
-  alert('You didnt make a choice');
+  alert('Sorry, you didnt make a choice or your value is not a number');
 }
